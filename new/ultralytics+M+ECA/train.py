ed0c7475a34c1d335ea7e02cdde83872e5291859
unknown <2210403072@qq.com>
2024-05-24 22:04:06 +0800

    yolo

---
+++ new/ultralytics+M+ECA/train.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8+MobileNetV3+ECA.yaml")  # build a new model from scratch
+
+
+# Use the model
+model.train(data="/root/autodl-tmp/RUOD_yolo/my_data.yaml", epochs=100)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
+# results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+path = model.export(format="onnx")  # export the model to ONNX format
